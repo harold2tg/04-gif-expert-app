60db86150a0098156e6a3d7bc356d48f5bda7a99
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-21 21:22:28 -0500

    80. GifGrid - Nuevo componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
 import {AddCategory} from '../src/components/AddCategory'
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'Naruto']);
-  const onAddCategory = () =>{
+  const onAddCategory = (newCategory) =>{
     //Priemra forma de agrear seria
     // setCategories([ ...categories, 'Caballeros del Zodiaco']);
    
     //segunda forma de agregar
     // setCategories(cat => [...cat, 'Caballeros del Zodiaco']);
-
+    // categories.push(newCategory)
+    
+    if (categories.includes(newCategory)){
+      alert("ya existe esta categoria")
+      return
+    };
+ 
     // para insertarlo al inicio 
-    setCategories(cat => ['Caballeros del Zodiaco',...cat]);
+    setCategories([newCategory,...categories]);
 
 
 
@@ -20,19 +27,42 @@ export const GifExpertApp = () => {
 
   return (
     <>
-    {/* Titulo */}
         <h1>GifExpertApp</h1>
-      {/* input */}
-  <AddCategory setCategories={setCategories}/>
+    <AddCategory
+      // setCategories={setCategories}
+      onNewCategory = { value => (onAddCategory(value))}
+      currentCategories = {categories}
+     />
     
     <button onClick={onAddCategory}>Agregar</button>
       {/* listados de Gif */}
-      <ol>
-        {categories.map( category =>{
-            return <li key={category}>{category}</li>
+        {/* {categories.map( category =>{
+            return (
+              <div  key={category}>
+                <h3>{category}</h3>
+                <li>{category}</li>
+              </div>
+            )
           })
-        }
-      </ol>
+        } */}
+
+        {/* otra orma de mostrar es  */}
+        
+        {/* {categories.map( (category) =>(
+              <div  key={category}>
+                <h3>{category}</h3>
+                <li>{category}</li>
+              </div>
+          ))
+        }   */}
+
+         {categories.map( (category) =>(
+              <GifGrid 
+                key={category}
+                category={category}
+              />
+          ))
+        }  
     </>
   )
 }
