6816e105b6cc399281355a316752a2c3a86c30e6
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-29 10:52:15 -0500

    Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,6 +9,21 @@ export const GifGrid = ({category}) => {
   return (
     <>
         <h3>{category}</h3>
+        {/* forma 1 para mostras cargando  */}
+        {/* {
+          isLoading 
+          ? <h2>Cargando...</h2>
+          :null
+
+        } */}
+
+        {/* forma 2 para mostrar el cargando */}
+
+        {
+          isLoading && <h2>Cargando...</h2>
+        }
+
+
         <div className="card-grid">
             {
                 images.map( ( image ) =>(
