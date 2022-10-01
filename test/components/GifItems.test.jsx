f5f88fe48e13d1a3b185b301b4da7a730441f818
Harold Torres Gallo <harold2tg@gmail.com>
2022-10-01 13:20:56 -0500

    102. Pruebas del componente - GifGridItem ok

--- test/components/GifItems.test.jsx
+++ test/components/GifItems.test.jsx
@@ -2,17 +2,33 @@
  * @jest-environment jsdom
  */
 
-import {render} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import {GifItems} from '../../src/components/GifItems';
 
 describe('Prueba en < GifItems >', () => { 
     
-    const mytitle = "Saitama";
-    const myurl   =   "https://one-match.com/saitam.jpg";
+    const title = "Saitama";
+    const url   =   "https://one-match.com/saitam.jpg";
 
     test('Debe hacer un match con el snapshop', () => { 
         // render(<GifItems/>)
-        const { container } = render(<GifItems title={ mytitle } url={myurl}/>)
+        const { container } = render(<GifItems title={ title } url={ url }/>)
         expect( container ).toMatchSnapshot();
      })
+     test('Debe mostrar la imagen con el url y el ALT indicado', () => { 
+        const { container } = render(<GifItems title={ title } url={ url }/>)
+        // screen.debug();
+        const {src,alt } = screen.getByRole('img')
+        expect(src).toBe(url)
+        expect(alt).toBe(alt)
+      })
+
+      test('Debemos mostrar el titulo en el componente', () => { 
+        
+        const { container } = render(<GifItems title={ title } url={ url }/>)
+        expect(screen.getByText(title)).toBeTruthy();
+
+
+       })
+
  })
