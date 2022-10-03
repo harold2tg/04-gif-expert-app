cbebb91b73cc226d731c281176ec7513554c355a
Harold Torres Gallo <harold2tg@gmail.com>
2022-10-03 15:35:47 -0500

    105. Simular un submit del formulario

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -13,6 +13,19 @@ describe('Pruebas en el <AddCategory/>', () => {
       fireEvent.input(input, {target:{value:'Saitama'}})
       // screen.debug();
       expect(input.value).toBe('Saitama')
+     });
+     test('Debe de llamar onNewCategory si el input tiene un valor', () => { 
 
-     })
+        const inputValue = 'Saitama';
+        render(<AddCategory onNewCategory = {()=> {}}/>) 
+        const input = screen.getByRole('textbox')
+        const form = screen.getByRole('form')
+        fireEvent.input(input, {target:{value:inputValue}})
+        fireEvent.submit(form)
+        expect(input.value).toBe('')
+
+
+
+      })
+
  })
